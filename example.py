a82b3580eea0a2bade5b9b3e9142c9e9023380e9
Old Mill <133509455+OneOldMill@users.noreply.github.com>
2023-12-17 20:39:33 -0500

    Add files via upload

--- example.py
+++ example.py
@@ -7,17 +7,17 @@ serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        #self.send_response(200)
-        self.send_response(200, "apple server 555")
+        self.send_response(200)
         #self.send_response(status.code, status.message)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
-        self.wfile.write(bytes("Hello, world!"))
+        #self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        #self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        #self.wfile.write(bytes("<body>", "utf-8"))
+        #self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        #self.wfile.write(bytes("</body></html>", "utf-8"))
+        #self.wfile.write(bytes("Hello, world!", "utf-8"))
+        return
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
