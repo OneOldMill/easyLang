25c2170c27569694b82bfea87a5473e668028356
Old Mill <133509455+OneOldMill@users.noreply.github.com>
2023-12-24 22:30:00 -0500

    Update example.py

--- example.py
+++ example.py
@@ -29,8 +29,7 @@ class MyServer(BaseHTTPRequestHandler):
         # read that many bytes from the body of the request
         body = self.rfile.read(content_length)
 
-        #result = input_process(body)
-        result = "apple - korean"
+        result = input_process(body)
           
         self.send_response(200)
         self.send_header("Access-Control-Allow-Origin", "*")
