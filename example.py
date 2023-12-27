0c33b8f2566b6a05829d4f888ffc1c50e22ad0e7
Old Mill <133509455+OneOldMill@users.noreply.github.com>
2023-12-27 15:14:55 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,44 +1,52 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from urllib.parse import urlparse
-import json
+# Python 3 server example
+# https://pythonbasics.org/webserver/
+from http.server import BaseHTTPRequestHandler, HTTPServer
+#import time
+#import db_select.py
+#from process import input_process
 
-class GetHandler(BaseHTTPRequestHandler):
+hostName = "localhost"
+serverPort = 8080
 
-    def do_GET(self):
-        parsed_path = urlparse(self.path)
-        message = '\n'.join([
-            'CLIENT VALUES:',
-            'client_address=%s (%s)' % (self.client_address,
-                self.address_string()),
-            'command=%s' % self.command,
-            'path=%s' % self.path,
-            'real path=%s' % parsed_path.path,
-            'query=%s' % parsed_path.query,
-            'request_version=%s' % self.request_version,
-            '',
-            'SERVER VALUES:',
-            'server_version=%s' % self.server_version,
-            'sys_version=%s' % self.sys_version,
-            'protocol_version=%s' % self.protocol_version,
-            '',
-            ])
+class MyServer(BaseHTTPRequestHandler):
+     def do_GET(self):
         self.send_response(200)
+        #self.send_response(status.code, status.message)
+        #self.send_header("Content-type", "text/html")
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
-        self.wfile.write(message.encode('utf-8'))
-        return
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>GET: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is do_GET of HTTP server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.close()
+        #self.wfile.write(bytes("Hello, world!", "utf-8"))
 
-    def do_POST(self):
-        content_len = int(self.headers.getheader('content-length'))
-        post_body = self.rfile.read(content_len)
+     def do_POST(self):
+        # read the content-length header
+        content_length = int(self.headers.get("Content-Length"))
+        # read that many bytes from the body of the request
+        body = self.rfile.read(content_length)
+
+        #result = input_process(body)
+        result = "test message from http server"
+
         self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
 
-        data = json.loads(post_body)
+        #self.wfile.write(body)
+        self.wfile.write(bytes(result, 'utf8'))
+    
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
 
-        self.wfile.write(data['foo'])
-        return
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
 
-if __name__ == '__main__':
-    server = HTTPServer(('localhost', 8080), GetHandler)
-    print('Starting server at http://localhost:8080')
-    server.serve_forever()
+    webServer.server_close()
+    print("Server stopped.")
